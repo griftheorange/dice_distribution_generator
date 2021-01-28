e492c31884a82c1b1e321bc444fe154bdc2519d9
griftheorange <griffinpoole5@gmail.com>
2021-01-28 16:36:35 -0500

    Cleaned up old code, added functionality to display combination method dice roll output in scatterplot in grid form

--- generator.py
+++ generator.py
@@ -5,26 +5,9 @@ import itertools
 import math
 
 
-def get_distribution(num, sides):
-    arr = []
-    results = {}
-    for n in range(0, sides**num):
-        print(n)
-        sum_val = sum([((n // (sides**m)) % sides)+1 for m in range(0,num)])
-        if sum_val in results.keys():
-            results[sum_val] += 1
-        else:
-            results[sum_val] = 1
-    
-    total = sum(results.values())
-
-    results = {k: float(v)/float(total)*100 for k,v in results.items()}
-    binomial_results = binomialize(results)
-
-    return [results, binomial_results, str(num)+"d"+str(sides)]
-
 def combinations_tool(num, sides):
     results = {}
+    line_results = []
     # Generate all combinations from set of possible die rolls, allow repeats
     combinations = list(itertools.combinations_with_replacement([i for i in range(1, sides+1)], num))
 
@@ -55,6 +38,9 @@ def combinations_tool(num, sides):
         # They will always sum to the same value naturally
         num_unique_perms = (math.factorial(len(comb))/denominator)
         print("Combination", comb, sum_val, num_unique_perms)
+
+        for i in range(0,int(num_unique_perms)):
+            line_results.append(sum_val)
         
         # Using the number of the unique terms, we can increment the result hash at the key of their sum
         if sum_val in results.keys():
@@ -64,10 +50,9 @@ def combinations_tool(num, sides):
             
     # Map all the values to their respective proportion vs the total, make percentage 
     total = sum(results.values())
-    print(results)
     results = {k: float(v)/float(total)*100 for k,v in results.items()}
     binomial_results = binomialize(results)
-    return [results, binomial_results, str(num)+"d"+str(sides)]
+    return [results, binomial_results, str(num)+"d"+str(sides), line_results]
         
 def binomialize(results):
     binomial_results = {}
@@ -87,34 +72,35 @@ def plot_graphs(data):
     axs[1].axhline(y=50, linewidth=1, color='b')
     axs[0].bar(data[0].keys(), data[0].values(), 0.7, color='g')
     axs[1].bar(data[1].keys(), data[1].values(), 0.7, color='r')
-    plt.show()
-        
-def experiment(num, sides):
-    val = num
-    counter = 1
-    results = {}
-    for i in range(0, sides**num):
-        if counter > sides:
-            counter = 1
-            val -= (sides-1)
-        
-        if val in results.keys():
-            results[val] += 1
-        else:
-            results[val] = 1
-        counter += 1
-        val += 1
-    print(results)
+    fig.show()
+
+def plot_line_grid(data_arr):
+    fig1, f1_axes = plt.subplots(nrows=len(data_arr), ncols=len(data_arr[0]))
+    for x in range(0, len(data_arr)):
+        for y in range(0, len(data_arr[x])):
+            f1_axes[x][y].scatter(data_arr[x][y][0], list(range(1, len(data_arr[x][y][0])+1)), color='b', s=2)
+            f1_axes[x][y].plot(data_arr[x][y][0], list(range(1, len(data_arr[x][y][0])+1)), color='b')
+            f1_axes[x][y].title.set_text(data_arr[x][y][1])
+    fig1.suptitle("Scatterplots of dice sum occurrences generated through combination method, lines connect in order of occurrence: " + str(len(data_arr))+'x'+str(len(data_arr[x])))
+    fig1.show()
 
 
 pattern = re.compile("^\d+d\d+$")
+pattern_linegrid = re.compile("^\d+x\d+$")
 while True:
     print("Please type your die:")
     value = input()
     if pattern.match(value):
         values = list(map(lambda x: int(x), value.split("d")))
-        # data = get_distribution(values[0], values[1])
         data = combinations_tool(values[0], values[1])
-        plot_graphs(data)
-        # experiment(values[0], values[1])
+        plot_graphs(  data)
+    elif pattern_linegrid.match(value):
+        values = list(map(lambda x: int(x), value.split("x")))
+        datasets = []
+        for x in range(0, values[0]):
+            datasets.append([])
+            for y in range(0, values[1]):
+                dataset = combinations_tool(x+1, y+1)
+                datasets[x].append([dataset[3], dataset[2]])
+        plot_line_grid(datasets)
 
